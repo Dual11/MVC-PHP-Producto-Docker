044ec65e5323d972af6c0b88f78a798dbf3ed205
alex01 <alextobar013@gmail.com>
2025-10-23 21:29:57 +0200

    done

--- sql/dev/products.sql
+++ sql/dev/products.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `PRODUCTS` (
-  `cod` INT AUTO_INCREMENT PRIMARY KEY,
-  `short_name` varchar(20) NOT NULL,
-  `pvp` decimal(5,2) NOT NULL,
-  `nombre` varchar(100) NOT NULL
+`cod` INT AUTO_INCREMENT PRIMARY KEY,
+`short_name` varchar(20) NOT NULL,
+`pvp` decimal(5,2) NOT NULL,
+`nombre` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `PRODUCTS` (`cod`, `short_name`, `pvp`, `nombre`) VALUES
